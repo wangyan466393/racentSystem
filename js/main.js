3d408c996bdf51465e1733c3b8876b8228d4bcdd
ljp <516805886@qq.com>
2020-08-04 18:55:10 +0800

    orderdetail

--- js/main.js
+++ js/main.js
@@ -1,56 +1,71 @@
-//需求：单击标题，让对于的内容显示
-        //tab切换
-        var as = document.querySelectorAll("#tab li a");
-        var lis = document.querySelectorAll("#tab li");
-        for (var i = 0; i < as.length; i++) {
-            //绑定事件
-            as[i].onclick = function () {
-                console.log(this)
-                //查找要修改的元素
-                //当触发a的时候 ，让所有的 div内容隐藏
-                var divs = document.querySelectorAll("#content>div");
-                for (var j = 0; j < divs.length; j++) {
-                    divs[j].style.display = "none";
-                    lis[j].className = ""
-                }
-                //进行修改
-                //让当前a对应的 div显示
-                var i = this.href.lastIndexOf("#");  //查找的就是#出现的位置；this.href  完整的路径地址；
-                var id = this.href.substring(i);   // 截取#到结尾的内容
-                console.log(document.querySelector(id).parentNode);  //#content1  #content2  #content3
-                document.querySelector(id).style.display = "block";
-                this.parentElement.className = "active";
-            }
+window.onload=function(){
+    var json = [
+        { "name": "ljp", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },   //json[0]
+        { "name": "ljp1", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },
+        { "name": "ljp2", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },
+        { "name": "ljp3", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },
+        { "name": "ljp4", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },
+    ];
+    //订单管理--所有订单信息
+    getTableData()
+    function getTableData() {
+        var tableInfo = ""
+        for (var i = 0; i < json.length; i++) {    //遍历Table的所有Row
+            tableInfo += "<tr>"
+                //遍历Row中的每一列
+                //获取Table中单元格的内容
+                + " <td>" + json[i].name + "</td> "
+                + "<td>20200803xcnjj63cm</td>"
+                + "<td>Sectigo EV代码签名</td>"
+                + "<td>2020-08-03 18:35:00</td>"
+                + "<td>零钱支付</td>"
+                + "<td>新</td>"
+                + "<td>草稿</td>"
+                + "<td>￥1299.60</td>"
+                + "<td><a data-id='orderDetail'>详情</a></td>  ";
+
+            tableInfo += "</tr>";
         }
+        $(".right_content #order_dataLists tbody").html(tableInfo)
+    }
 
-        var json = [
-            { "name": "ljp", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },   //json[0]
-            { "name": "ljp1", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },
-            { "name": "ljp2", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },
-            { "name": "ljp3", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },
-            { "name": "ljp4", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "order_date": new Date(), "payment_method": "零钱支付", "order_type": "新", "order_status": "草稿", "order_price": "1229.60" },
-        ];
+    //获取订阅管理-所有订单
+    var jsonSub = [
+        { "name": "ljp", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "past_date": new Date(), "renewal_cycle": "1年", "order_price": "1229.60" ,"product_status":"申请中"},   //json[0]
+        { "name": "ljp1", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "past_date": new Date(), "renewal_cycle": "1年", "order_price": "1229.60" ,"product_status":"申请中"},
+        { "name": "ljp2", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "past_date": new Date(), "renewal_cycle": "1年",  "order_price": "1229.60" ,"product_status":"申请中"},
+        { "name": "ljp3", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "past_date": new Date(), "renewal_cycle": "1年", "order_price": "1229.60","product_status":"申请中" },
+        { "name": "ljp4", "order_num": "20200803xcnjj63cm", "product_name": "Sectigo EV代码签名", "past_date": new Date(), "renewal_cycle": "1年", "order_price": "1229.60" ,"product_status":"申请中"},
+    ];
+    getSubscriptionData()
+    function getSubscriptionData() {
+        var tableInfo = ""
+        for (var i = 0; i < jsonSub.length; i++) {    //遍历Table的所有Row
+            tableInfo += "<tr>"
+                //遍历Row中的每一列
+                //获取Table中单元格的内容
+                + " <td>" + jsonSub[i].name + "</td> "
+                + "<td>" + jsonSub[i].order_num + "</td>"
+                + "<td>" + jsonSub[i].product_name + "</td>"
+                + "<td>2020-08-03 18:35:00</td>"
+                + "<td>" + jsonSub[i].renewal_cycle + "</td>"
+                + "<td>" + jsonSub[i].order_price + "</td>"
+                + "<td>" + jsonSub[i].product_status + "</td>"
+                + "<td><a data-id='orderDetail'><img src='images/delete.png'></a></td>  ";
 
-        //获取所有订单表数据
-        getTableData()
-        function getTableData() {
-            var tableInfo = "";
-            var tbody = document.getElementsByTagName("tbody")[0];
-            for (var i = 0; i < json.length; i++) {    //遍历Table的所有Row
-                tableInfo += "<tr>"
-                    //遍历Row中的每一列
-                    //获取Table中单元格的内容
-                    + " <td>"+json[i].name+"</td> "
-                    + "<td>20200803xcnjj63cm</td>"
-                    + "     <td>Sectigo EV代码签名</td>"
-                    + "     <td>2020-08-03 18:35:00</td>"
-                    + "       <td>零钱支付</td>"
-                    + "       <td>新</td>"
-                    + "       <td>草稿</td>"
-                    + "       <td>￥1299.60</td>"
-                    + "       <td><a href='#'>详情</a></td>  ";
-
-                + "</tr>";
-            }
-            tbody.innerHTML = tableInfo ;
+            tableInfo += "</tr>";
         }
+        $(".right_content #subscription_dataLists tbody").html(tableInfo)
+    }
+}
+   
+
+
+
+
+
+
+
+
+
+
--- js/tab.js
+++ js/tab.js
@@ -1,23 +1,58 @@
-$(function(){
-    $(".leftMenus").on("click", "li", function(){
+
+$(function () {
+    $(".leftMenus").on("click", "li", function (e) {
+        
+        var sId = $(this).data("id");  //获取data-id的值
+        window.location.hash = sId;  //设置锚点
+        loadInner(sId);
+        
+    });
+    $(".nav .select_menus").on("click", "li", function (e) {
+        e.preventDefault()
+        var sId = $(this).data("id");  //获取data-id的值
+        window.location.hash = sId;  //设置锚点
+        loadInner(sId);
+        
+    });
+    //订单详情
+    $(".right_content").on("click", "#order_dataLists a", function (e) {
+        e.preventDefault();
+        console.log(e)
         var sId = $(this).data("id");  //获取data-id的值
         window.location.hash = sId;  //设置锚点
         loadInner(sId);
     });
-
-    function loadInner(sId){
+    function loadInner(sId) {
         var sId = window.location.hash;
         var pathn, i;
-        switch(sId){
+        switch (sId) {
             case "#orderManagement": pathn = "orderManagement.html"; i = 0; break;
-// 　　　　　　　case "#account": pathn = "user_account.html"; i = 1; break;
-//             case "#trade": pathn = "user_trade.html"; i = 2; break;
-//             case "#info": pathn = "user_info.html"; i = 3; break;
-// 　　　　　　  default: pathn = "user_center.html"; i = 0; break;
+            case "#orderDetail": pathn = "orderDetail.html"; break;
+            case "#subscriptionManagement": pathn = "subscriptionManagement.html"; break;
+            //             case "#info": pathn = "user_info.html"; i = 3; break;
+            default: pathn = "subscriptionManagement.html"; i = 0; break;
         }
         $(".right_content").load(pathn); //加载相对应的内容
         $(".leftMenus li").eq(i).addClass("active").siblings().removeClass("active"); //当前列表高亮
     }
     var sId = window.location.hash;
     loadInner(sId);
+
+    //tab切换
+    $('.right_content').on('click', '#tab li', function (e) {
+        var i = $(this).index()
+        e.preventDefault()
+        $('#tab li').removeClass("active");
+        $(".order_content").css('display', 'none');
+        $("#content .order_content").eq(i).css('display', 'block');
+        $(this).addClass("active");
+    })
+
+
+    
+
+
+
 })
+
+
