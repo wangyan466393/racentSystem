7bd3e8b977be2a797e72e2980840cf5f3e088d4c
ljp <516805886@qq.com>
2020-08-05 18:33:53 +0800

    订单管理

--- js/tab.js
+++ js/tab.js
@@ -1,4 +1,5 @@
-
+console.log($(document).scrollTop())
+console.log($(".left_lists").height() + $(document).scrollTop())
 $(function () {
     $(".leftMenus").on("click", "li", function (e) {  
         var sId = $(this).data("id");  //获取data-id的值
@@ -26,7 +27,7 @@ $(function () {
             case "#orderManagement": pathn = "orderManagement.html"; i = 0; break;
             case "#orderDetail": pathn = "orderDetail.html"; break;
             case "#subscriptionManagement": pathn = "subscriptionManagement.html"; break;
-            //             case "#info": pathn = "user_info.html"; i = 3; break;
+            case "#customerData": pathn = "customerData.html"; break;
             default: pathn = "subscriptionManagement.html"; i = 0; break;
         }
         $(".right_content").load(pathn); //加载相对应的内容
