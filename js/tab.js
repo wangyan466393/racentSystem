c8c863bdc565ac032c9120e72d1beb0088a1c357
ljp <516805886@qq.com>
2020-08-07 12:52:23 +0800

    ljp

--- js/tab.js
+++ js/tab.js
@@ -1,10 +1,12 @@
 $(function () {
+    //常用工具-菜单
     $(".leftMenus").on("click", "li", function (e) {  
         var sId = $(this).data("id");  //获取data-id的值
         window.location.hash = sId;  //设置锚点
         loadInner(sId);
+        $(".nav li[data-id='"+sId+"']").addClass('active').siblings().removeClass('active');
     });
-     
+     //导航
     $(".nav").on("click", "li", function (e) {
         e.preventDefault();
         e.stopPropagation();    //  阻止事件冒泡
@@ -25,10 +27,13 @@ $(function () {
         var sId = window.location.hash;
         var pathn, i;
         switch (sId) {
+            //销售管理
             case "#orderManagement": pathn = "orderManagement.html"; i = 0; break;
             case "#orderDetail": pathn = "orderDetail.html"; break;
             case "#subscriptionManagement": pathn = "subscriptionManagement.html"; break;
             case "#customerData": pathn = "customerData.html"; break;
+            //商品管理
+            case "#commodityManagement": pathn = "commodityManagement.html";i=5; break;
             // 财务管理
             case "#finance": pathn = "finance.html";i=1; break;
             default: pathn = "subscriptionManagement.html"; i = 0; break;
@@ -42,11 +47,24 @@ $(function () {
     //tab切换右侧
     $('.right_content').on('click', '#tab li', function (e) {
         var i = $(this).index()
+        console.log(i)
         e.preventDefault()
         $('#tab li').removeClass("active");
         $(".order_content").css('display', 'none');
         $("#content .order_content").eq(i).css('display', 'block');
         $(this).addClass("active");
+        // 显示隐藏-客户资料按钮
+        if (i == 1) {
+            $(".right_content .personal_box_btn").css('display', 'block')
+        }else {
+            $(".right_content .personal_box_btn").css('display', 'none')
+        }
+        if (i == 5) {
+            $(".right_content .level_box").css('display', 'block')
+        }else{
+            $(".right_content .level_box").css('display', 'none')
+        }
+        
     })
     
 
